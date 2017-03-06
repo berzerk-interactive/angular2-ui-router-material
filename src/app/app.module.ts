c64696ad4e812577ee2bf2598bde2eb72b911036
Lee <leeblazek@gmail.com>
2017-03-06 14:25:49 -0700

    added material and its working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { SidenavOverviewExample } from './sidenav/sidenav-overview-example';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavOverviewExample
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
